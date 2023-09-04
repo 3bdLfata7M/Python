fc05baea2631cef1382e232f153a762c45a1ca1a
3bdLfata7M <127152165+3bdLfata7M@users.noreply.github.com>
2023-09-04 16:16:47 +0300

    upload calculator main.py and clac.py files

---
+++ main.py
@@ -0,0 +1,37 @@
+from calc import add, sub, mult, div
+
+
+def calc():
+    firstNumber = int(input("enter first Number !! "))
+    operation = input("enter the operation !! ")
+    secondNumber = int(input("enter second Number !! "))
+
+    if operation == "add" :
+        result = add(firstNumber, secondNumber)
+    elif operation == "sub" :
+        result = sub(firstNumber, secondNumber)
+
+    elif operation == "mult" :
+        result = mult(firstNumber, secondNumber)
+
+    elif operation == "div" :
+        result = div(firstNumber, secondNumber)
+
+    else:
+        result = "wrong operation"
+
+    print(result)
+
+    anotherOperation = input("Another Operation : yes or No ? ")
+    if anotherOperation == "yes" or anotherOperation == "YES" :
+        calc()
+    
+
+    
+
+calc()
+
+
+
+
+
